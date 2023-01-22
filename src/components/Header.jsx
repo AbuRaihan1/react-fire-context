ca2eee5b182805be9b447c18471d642e1d5ce0a8
Abu Raihan <raihanahmed01973@gmail.com>
2023-01-22 12:23:16 +0600

    done login page ui

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,21 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div>
-      <div className="navbar bg-primary text-primary-content">
-        <Link to='/' className="btn btn-ghost normal-case text-xl">daisyUI</Link>
-        <Link to='/home' className="btn btn-ghost normal-case text-l">Home</Link>
-        <Link to='/register' className="btn btn-ghost normal-case text-l">Register</Link>
-        <Link to='/login' className="btn btn-ghost normal-case text-l" >Log in</Link>
+      <div className="navbar bg-primary text-primary-content d-flex justify-between">
+        <Link to="/" className="btn btn-ghost normal-case text-xl">
+          daisyUI
+        </Link>
+        <div>
+          <Link to="/home" className="btn btn-ghost normal-case text-l">
+            Home
+          </Link>
+          <Link to="/register" className="btn btn-ghost normal-case text-l">
+            Register
+          </Link>
+          <Link to="/login" className="btn btn-ghost normal-case text-l">
+            Log in
+          </Link>
+        </div>
       </div>
     </div>
   );
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,7 +3,46 @@ import React from "react";
 const Login = () => {
   return (
     <div>
-      
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold pb-3">Wellcome back</h1>
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <div className="card-body">
+              <div className="form-control">
+                <h2 className="text-3xl mb-4">Login here</h2>
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="email"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="password"
+                  className="input input-bordered"
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Login</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
